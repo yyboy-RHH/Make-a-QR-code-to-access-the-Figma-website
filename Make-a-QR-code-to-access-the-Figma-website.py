d7a5df8ab4b120d52376282f8dd577bed02bc242
RONG HSUAN, HSU <73116334+yyboy-RHH@users.noreply.github.com>
2025-08-05 18:19:44 +0800

    Add files via upload
    upload text file

---
+++ Make-a-QR-code-to-access-the-Figma-website.py
@@ -0,0 +1,29 @@
+
+'''
+如何製作可以連上Figma網站的QR code??
+
+1.打開免費 QR Code 產生網站（例如）：
+•	QR Code Generator
+•	QR Code Monkey
+
+2.複製 Figma 網址
+    例如你的 Figma 網址是：
+    https://www.figma.com/file/xxxxxxxx/ProjectName
+
+3.在QR Code 產生器網站的網址欄貼上 Figma 網址 → 選擇 URL 模式
+
+4.點選「Generate / 產生」QR Code
+
+5.下載 PNG 或 SVG 檔即可使用。
+
+注意事項
+•	如果是 Figma 檔案連結，請確認分享權限：
+  o	你需要將檔案設為 Anyone with the link can view（持連結者可檢視），這樣掃描 QR Code 的人才能打開。
+•	如果是 Figma 社群作品（Community），公開連結本身就能直接存取。
+
+
+Figma 連結需要設為「Anyone with the link can view（持連結者可檢視）怎麼設定?
+
+我來詳細教你 如何把 Figma 連結設定成「Anyone with the link can view（持連結者可檢視）」，這樣別人才可以掃 QR Code 直接開啟檔案：
+
+'''
